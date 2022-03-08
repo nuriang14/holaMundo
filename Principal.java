ef896a466bc29fae4e4cc5ac82785d505d2f6127
nuriang14 <101175013+nuriang14@users.noreply.github.com>
2022-03-08 11:56:12 +0100

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,5 +1,5 @@
 public class Principal{
-public static void main(string [] args){
-System.out.println("Hola mundo");
-}
+   public static void main(string [] args){
+   System.out.println("Hola mundo");
+   }
 }
